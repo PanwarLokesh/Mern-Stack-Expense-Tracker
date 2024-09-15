4393ce60526c5360b782993a2f8774076f44c1a9
lokesh <lokesh13122003@gmail.com>
2024-09-15 22:44:12 +0530

    add update and delete controller

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const Category = require("../model/Category");
+const Transaction = require("../model/Transaction");
 
 const categoryController = {
   //!adding
@@ -36,6 +37,57 @@ const categoryController = {
     const categories = await Category.find({ user: req.user });
     res.status(200).json(categories);
   }),
+
+  update: asyncHandler(async (req, res) => {
+    const {id:categoryId} = req.params;
+    const { type, name } = req.body;
+    const normalizedName = name.toLowerCase();
+    const category = await Category.findById(categoryId);
+    if (!category && category.user.toString() !== req.user.toString()) {
+      res.json({ message: "Category not found or user not authorized" });
+    }
+    const oldName = category.name;
+    category.name = normalizedName|| category.name; ;
+    category.type = type || category.type;
+    const updatedCategory = await category.save();
+    if (updatedCategory.name !== oldName) {
+      await Transaction.updateMany(
+        {
+          user: req.user,
+          category: oldName,
+        },
+        {
+          $set: {
+            category: updatedCategory.name,
+          },
+        }
+      );
+    }
+    res.json(updatedCategory);
+  }),
+
+  delete: asyncHandler(async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if (category && category.user.toString() === req.user.toString()) {
+      const defaultCategory = "Uncategorized";
+      await Transaction.updateMany(
+        {
+          user: req.user,
+          category: category.name,
+        },
+        {
+          $set: {
+            category: defaultCategory,
+          },
+        }
+      );
+      await Category.findByIdAndDelete(req.params.id);
+      res.json({message: "Category deleted successfully and transactions updated"});
+    }
+    else{
+      res.json({message: "Category not found or user not authorized"});
+    }
+  }),
 };
 
-module.exports= categoryController;
+module.exports = categoryController;
