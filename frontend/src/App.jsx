704d55269e6c7ddffad5593708cf755e3152a995
lokesh <lokesh13122003@gmail.com>
2024-09-22 12:42:54 +0530

    add Home, Login, Register components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import {BrowserRouter , Routes, Route} from "react-router-dom";
+import HeroSection from "./components/Home/HomePage";
+import PublicNavbar from "./components/Navbar/PublicNavbar";
+import LoginForm from "./components/Users/Login";
+import RegistrationForm from "./components/Users/Register";
 function App() {
   return (
-    <>
-      <h1 className="text-3xl  text-red-200 font-bold underline">Hello world!</h1>
-    </>
+    <BrowserRouter>
+    <PublicNavbar/>
+      <Routes>
+        <Route path="/"  element={<HeroSection/>}/>
+        <Route path="/login"  element={<LoginForm/>}/>
+        <Route path="/register"  element={<RegistrationForm/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
