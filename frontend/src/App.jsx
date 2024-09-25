e034879f1414fce6144dcdd74f76cee012fea839
lokesh <lokesh13122003@gmail.com>
2024-09-25 17:02:56 +0530

    add, list, update and delete categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import RegistrationForm from "./components/Users/Register";
 import { getUserFromStorage } from "./utils/getUserFromStorage";
 import PrivateNavbar from "../Templates/Navbar/PrivateNavbar";
 import { useSelector } from "react-redux";
+import AddCategory from "./components/Category/AddCategory";
+import CategoriesList from "./components/Category/CategoriesList";
+import UpdateCategory from "./components/Category/UpdateCategory";
 function App() {
   const user  = useSelector((state)=> state?.auth?.user);
   
@@ -16,6 +19,9 @@ function App() {
         <Route path="/"  element={<HeroSection/>}/>
         <Route path="/login"  element={<LoginForm/>}/>
         <Route path="/register"  element={<RegistrationForm/>}/>
+        <Route path="/add-category"  element={<AddCategory/>}/>
+        <Route path="/categories"  element={<CategoriesList/>}/>
+        <Route path="/update-category/:id"  element={<UpdateCategory/>}/>
       </Routes>
     </BrowserRouter>
   );
